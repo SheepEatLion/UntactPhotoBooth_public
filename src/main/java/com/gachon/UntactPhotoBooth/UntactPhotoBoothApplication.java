cf8507701b9208fdab856b48b27461f2e652f676
SheepEatLion <gowjr207@gmail.com>
2021-10-26 22:17:25 +0900

    Changes

--- src/main/java/com/gachon/UntactPhotoBooth/UntactPhotoBoothApplication.java
+++ src/main/java/com/gachon/UntactPhotoBooth/UntactPhotoBoothApplication.java
@@ -3,9 +3,10 @@ package com.gachon.UntactPhotoBooth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UntactPhotoBoothApplication {
+public class UntactPhotoBoothApplication extends SpringBootServletInitializer {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.properties,"
@@ -18,6 +19,11 @@ public class UntactPhotoBoothApplication {
 		new SpringApplicationBuilder(UntactPhotoBoothApplication.class)
 				.properties(APPLICATION_LOCATIONS)
 				.run(args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(UntactPhotoBoothApplication.class);
 	}
 
 }
