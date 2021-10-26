5a31305ef31dc5a097b667e57339759b475859d0
SheepEatLion <gowjr207@gmail.com>
2021-10-26 21:37:18 +0900

    Changes

--- src/main/java/com/gachon/UntactPhotoBooth/Controller/HistoryController.java
+++ src/main/java/com/gachon/UntactPhotoBooth/Controller/HistoryController.java
@@ -9,7 +9,6 @@ import com.gachon.UntactPhotoBooth.util.ResponseUtil;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/gachon/UntactPhotoBooth/Controller/WebController.java
+++ src/main/java/com/gachon/UntactPhotoBooth/Controller/WebController.java
@@ -3,11 +3,9 @@ package com.gachon.UntactPhotoBooth.Controller;
 import com.gachon.UntactPhotoBooth.Configuration.Oauth.LoginUser;
 import com.gachon.UntactPhotoBooth.Configuration.Oauth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
